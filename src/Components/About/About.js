4d6e5c06e78730f33ad315a8b569880b056c0671
AliEl-Zayat <110483901+AliEl-Zayat@users.noreply.github.com>
2023-02-11 19:32:51 +0200

    latests

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -1,135 +1,141 @@
 import { Container } from "@mui/system";
 import "./About.css";
-import React,{useEffect} from "react";
+import React, { useEffect } from "react";
 import HeadingOfSection from "../HeadingOfSection/HeadingOfSection";
 import { Row, Col } from "react-bootstrap";
 import profilePicture from "./images/IMG_1103.JPG";
-import {FaLockOpen,FaUserCheck,FaHeadset,FaLaughBeam} from 'react-icons/fa'
+import {
+  FaLockOpen,
+  FaUserCheck,
+  FaHeadset,
+  FaLaughBeam,
+} from "react-icons/fa";
 import { Fade } from "react-awesome-reveal";
 const About = () => {
   const counterss = () => {
-    let valueDisplays = document.querySelectorAll('.nums');
+    let valueDisplays = document.querySelectorAll(".nums");
     let interval = 1000;
-    valueDisplays.forEach((valueDisplay) =>{
+    valueDisplays.forEach((valueDisplay) => {
       let startValue = 0;
-      let endValue = parseInt(valueDisplay.getAttribute('data-val'));
+      let endValue = parseInt(valueDisplay.getAttribute("data-val"));
       let duration = Math.floor(interval / endValue);
       let counter = setInterval(function () {
         startValue += 1;
         valueDisplay.textContent = startValue;
-        if (startValue === endValue){
+        if (startValue === endValue) {
           clearInterval(counter);
         }
-      },duration)
-    })
-  }
+      }, duration);
+    });
+  };
   useEffect(() => {
     counterss();
-  },[]);
+  }, []);
   return (
     <Container>
       <HeadingOfSection sectionName="About" sectionPara="LEARN MORE ABOUT ME" />
       <Row>
         <Col lg="4">
-        <Fade direction='left' triggerOnce='true'>
-          <div className="profile-image-holder">
-            <img
-              src={profilePicture}
-              className="img-fluid"
-              alt="profile"
-              width={"280px"}
-            />
-          </div>
+          <Fade direction="left" triggerOnce="true">
+            <div className="profile-image-holder">
+              <img
+                src={profilePicture}
+                className="img-fluid"
+                alt="profile"
+                width={"280px"}
+              />
+            </div>
           </Fade>
         </Col>
         <Col className="text-white">
-        <Fade direction='right' triggerOnce='true'>
-          <strong className="accent-clr jtitle">Full Stack Delveloper</strong>
-          <br />
-          <em className="statement">
-            Full Stack PHP Developer uses React as a frontend Libaray, mySQl for
-            database, and besides Bootstrap and other Libaray for CSS.
-          </em>
-          <div className="lists d-flex">
-            <ul>
-              <li>Freelance: Available</li>
-              <li>
-                {" "}
-                Phone:{" "}
-                <a
-                  className="text-decoration-none accent-clr"
-                  href="Tel:+201122006273"
-                >
+          <Fade direction="right" triggerOnce="true">
+            <strong className="accent-clr jtitle">Front End Delveloper</strong>
+            <br />
+            <em className="statement">
+              Front-End Developer uses React as a frontend Libaray,Redux for
+              Global state management and besides Bootstrap,ChakraUI,MUI and
+              other Libaray for CSS.
+            </em>
+            <div className="lists d-flex">
+              <ul>
+                <li>Freelance: Available</li>
+                <li>
                   {" "}
-                  +201122006273
-                </a>
-              </li>
-              <li>Experience: 1+ Year</li>
-            </ul>
-            <ul>
-              <li>Address: Menia ElQmh,Sharkia,Egypt</li>
-              <li>Degree: Bachelor</li>
-              <li>Hobby: Coding</li>
-            </ul>
-          </div>
-          <p className="brief">
-            I'm looking forward to enhancing my skills through working in
-            challengable projects with a professional team, I depend mostly on
-            self-learning to gain new skills
-          </p>
+                  Phone:{" "}
+                  <a
+                    className="text-decoration-none accent-clr"
+                    href="Tel:+201122006273"
+                  >
+                    {" "}
+                    +201122006273
+                  </a>
+                </li>
+                <li>Experience: 1+ Year</li>
+              </ul>
+              <ul>
+                <li>Address: Menia ElQmh,Sharkia,Egypt</li>
+                <li>Degree: Bachelor</li>
+                <li>Hobby: Coding</li>
+              </ul>
+            </div>
+            <p className="brief">
+              I'm looking forward to enhancing my skills through working in
+              challengable projects with a professional team, I depend mostly on
+              self-learning to gain new skills
+            </p>
           </Fade>
         </Col>
       </Row>
-        <Fade direction='up' triggerOnce='true'>
-      <Row>
-        <Col lg='3' md='6' sm='12'>
-          <div className="dcard">
-          <span className="abs-card">  
-            <FaLockOpen/>
-          </span>
-          <h3 className="nums text-white" data-val='70+'>
-            0
-          </h3>
-          <p className="accent-clr">Projects</p>
+      <Fade direction="up" triggerOnce="true">
+        <Row>
+          <Col lg="3" md="6" sm="12">
+            <div className="dcard">
+              <span className="abs-card">
+                <FaLockOpen />
+              </span>
+              <h3 className="nums text-white" data-val="70+">
+                0
+              </h3>
+              <p className="accent-clr">Projects</p>
             </div>
-        </Col>
-        <Col lg='3' md='6' sm='12'>
-        <div className="dcard">
-          <span className="abs-card">
-            <FaUserCheck/>
-          </span>
-          <h3 className="nums text-white" data-val='5'>
-            0
-          </h3>
-          <p className="accent-clr">Clients</p>
-        </div>
-        </Col>
-        <Col lg='3' md='6' sm='12'>
-        <div className="dcard">
-
-          <span className="abs-card">
-            <FaHeadset/>
-          </span>
-          <h3 className="nums text-white" data-val='300'>
-            0
-          </h3>
-          <p className="accent-clr">Hours Of Support</p>
-        </div>
-        </Col>
-        <Col lg='3' md='6' sm='12'>
-        <div className="dcard">
-
-          <span className="abs-card">
-            <FaLaughBeam/>
-          </span>
-          <h3 className="nums text-white" data-val='30'>
-            0
-          </h3>
-          <p className="accent-clr" id="happSup">Happy Supported</p>
-        </div>
-        </Col>
-      </Row>
-        </Fade>
+          </Col>
+          <Col lg="3" md="6" sm="12">
+            <div className="dcard">
+              <span className="abs-card">
+                <FaUserCheck />
+              </span>
+              <h3 className="nums text-white" data-val="5">
+                0
+              </h3>
+              <p className="accent-clr">Clients</p>
+            </div>
+          </Col>
+          <Col lg="3" md="6" sm="12">
+            <div className="dcard">
+              <span className="abs-card">
+                <FaHeadset />
+              </span>
+              <h3 className="nums text-white" data-val="300">
+                0
+              </h3>
+              <p className="accent-clr">Hours Of Support</p>
+            </div>
+          </Col>
+          <Col lg="3" md="6" sm="12">
+            <div className="dcard">
+              <span className="abs-card">
+                <FaLaughBeam />
+              </span>
+              <h3 className="nums text-white" data-val="30">
+                0
+              </h3>
+              <p className="accent-clr" id="happSup">
+                Happy Supported
+              </p>
+            </div>
+          </Col>
+        </Row>
+      </Fade>
     </Container>
   );
 };
--- src/Components/TimeLine/TimeLine.jsx
+++ src/Components/TimeLine/TimeLine.jsx
@@ -1,63 +1,79 @@
-import React from 'react'
-import { VerticalTimeline, VerticalTimelineElement }  from 'react-vertical-timeline-component';
-import 'react-vertical-timeline-component/style.min.css';
-import {MdSchool,MdWork} from 'react-icons/md'
-import {AiFillStar} from 'react-icons/ai'
+import React from "react";
+import {
+  VerticalTimeline,
+  VerticalTimelineElement,
+} from "react-vertical-timeline-component";
+import "react-vertical-timeline-component/style.min.css";
+import { MdSchool, MdWork } from "react-icons/md";
+import { AiFillStar } from "react-icons/ai";
 const TimeLine = () => {
   return (
-<VerticalTimeline>
-  <VerticalTimelineElement
-    className="vertical-timeline-element--work"
-    contentStyle={{ background: 'var(--main-color)', color: '#121212' }}
-    contentArrowStyle={{ borderRight: '7px solid  rgb(33, 150, 243)' }}
-    date="2021 - present"
-    iconStyle={{ background: 'var(--main-color)', color: '#fff' }}
-    icon={<MdWork />}
-  >
-    <h3 className="vertical-timeline-element-title">Full-Stack Developer</h3>
-    <h4 className="vertical-timeline-element-subtitle">HTML5 - CSS3 - Javascript - React JS</h4>
-    <p>
-      User Experience, Visual Designs, Effectient Worker and Committed person
-    </p>
-  </VerticalTimelineElement>
-  <VerticalTimelineElement
-    className="vertical-timeline-element--work"
-    date="2019 - 2020"
-    iconStyle={{ background: '#121212', color: 'var(--main-color)' }}
-    icon={<MdSchool />}
-  >
-    <h3 className="vertical-timeline-element-title text-black">Arduino Multi -Tasking Armed Robot </h3>
-    <h4 className="vertical-timeline-element-subtitle text-black">C++ Arduino</h4>
-    <p className='text-black'>
-      Creative Direction, Mechanics , Visual Capture ,Fully Functional
-    </p>
-  </VerticalTimelineElement>
-  <VerticalTimelineElement
-    className="vertical-timeline-element--work"
-    date="2019 - 2020"
-    iconStyle={{ background: '#121212', color: 'var(--main-color)' }}
-    icon={<AiFillStar />}
-  >
-    <h3 className="vertical-timeline-element-title text-black">Oracle DB for student</h3>
-    <h4 className="vertical-timeline-element-subtitle text-black">Cloud Orcale Database</h4>
-    <p className='text-black'>
-      Cloud Platform , Database
-    </p>
-  </VerticalTimelineElement>
-  <VerticalTimelineElement
-    className="vertical-timeline-element--work"
-    date="2018 - 2019"
-    iconStyle={{ background: '#121212', color: 'var(--main-color)' }}
-    icon={<AiFillStar />}
-  >
-    <h3 className="vertical-timeline-element-title text-black">Database Management Application</h3>
-    <h4 className="vertical-timeline-element-subtitle text-black">C# with SQL</h4>
-    <p className='text-black'>
-      Visual Design , Graphical User Interface , Database
-    </p>
-  </VerticalTimelineElement>
-</VerticalTimeline>
-  )
-}
+    <VerticalTimeline>
+      <VerticalTimelineElement
+        className="vertical-timeline-element--work"
+        contentStyle={{ background: "var(--main-color)", color: "#121212" }}
+        contentArrowStyle={{ borderRight: "7px solid  rgb(33, 150, 243)" }}
+        date="2021 - present"
+        iconStyle={{ background: "var(--main-color)", color: "#fff" }}
+        icon={<MdWork />}
+      >
+        <h3 className="vertical-timeline-element-title">Frot-end Developer</h3>
+        <h4 className="vertical-timeline-element-subtitle">
+          HTML5 - CSS3 - Javascript - React JS
+        </h4>
+        <p>
+          User Experience, Visual Designs, Effectient Worker and Committed
+          person
+        </p>
+      </VerticalTimelineElement>
+      <VerticalTimelineElement
+        className="vertical-timeline-element--work"
+        date="2019 - 2020"
+        iconStyle={{ background: "#121212", color: "var(--main-color)" }}
+        icon={<MdSchool />}
+      >
+        <h3 className="vertical-timeline-element-title text-black">
+          Arduino Multi -Tasking Armed Robot{" "}
+        </h3>
+        <h4 className="vertical-timeline-element-subtitle text-black">
+          C++ Arduino
+        </h4>
+        <p className="text-black">
+          Creative Direction, Mechanics , Visual Capture ,Fully Functional
+        </p>
+      </VerticalTimelineElement>
+      <VerticalTimelineElement
+        className="vertical-timeline-element--work"
+        date="2019 - 2020"
+        iconStyle={{ background: "#121212", color: "var(--main-color)" }}
+        icon={<AiFillStar />}
+      >
+        <h3 className="vertical-timeline-element-title text-black">
+          Oracle DB for student
+        </h3>
+        <h4 className="vertical-timeline-element-subtitle text-black">
+          Cloud Orcale Database
+        </h4>
+        <p className="text-black">Cloud Platform , Database</p>
+      </VerticalTimelineElement>
+      <VerticalTimelineElement
+        className="vertical-timeline-element--work"
+        date="2018 - 2019"
+        iconStyle={{ background: "#121212", color: "var(--main-color)" }}
+        icon={<AiFillStar />}
+      >
+        <h3 className="vertical-timeline-element-title text-black">
+          Database Management Application
+        </h3>
+        <h4 className="vertical-timeline-element-subtitle text-black">
+          C# with SQL
+        </h4>
+        <p className="text-black">
+          Visual Design , Graphical User Interface , Database
+        </p>
+      </VerticalTimelineElement>
+    </VerticalTimeline>
+  );
+};
 
-export default TimeLine
+export default TimeLine;
